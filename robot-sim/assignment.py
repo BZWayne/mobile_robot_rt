e8d0add862af23b3bbba7fbd595a571a537f5081
BZWayne <bauyrzhan.zhakanov@gmail.com>
2021-11-11 04:06:29 +0100

    comments

--- robot-sim/assignment.py
+++ robot-sim/assignment.py
@@ -9,16 +9,21 @@ from sr.robot import *
 d_th = 0.4
 a_th = 2
 silver = True
-dist_detect = 1.5
-dist_rot = 100
+dist_detect = 1.5                # a distance to detect silver or gold 
+dist_rot = 100                   # a distance to detect the boxes in rotation
+
+## Distances used to find obstacles, silver boxes
 diff = 20
 mid_point = 90
 search_s = 70
 search_g = 45
+##################################################
+
 diff_left = mid_point + diff     # difference of search from right
 diff_right = mid_point - diff    # difference of search from left
 angle_s = 30                     # search angle
 
+# Starting point
 R = Robot()
 
 """
@@ -108,7 +113,7 @@ def boxes(distance, angle):
 """
     Function to estimate the distance to the golden box. 
     Arguments:
-	distance (float): distance of the closest wall (if the distance is -1 and less or equal than 2.0)
+	distance (float): distance of the closest wall (if the distance is -1 and less or equal than 1.5)
     Returns:
     True if the angle is negative, then the robot turns left
     False if the angle is positive, then the robot turns right
@@ -116,33 +121,45 @@ def boxes(distance, angle):
 
 def rotate():
     dist = dist_rot
+    # Detecting any obstacles from the sides to avoid it by rotating 
     for token in R.see():
         if (-diff_left<token.rot_y<-diff_right or diff_right<token.rot_y<diff_left) and token.dist < dist and token.info.marker_type is MARKER_TOKEN_GOLD:
-            dist=token.dist
-            rot_y=token.rot_y
+            dist = token.dist
+            rot_y = token.rot_y
+    # Turning right with a speed of 25 in 0.1 sec
     if rot_y < 0:
         turn(25, 0.1)
         return False
+    # Turning left with a speed of 25 in 0.1 sec
     else:
         turn(-25, 0.1)
         return True
 
 #### Where the main action happens
+#### Will be run until the user terminates the program
 
 while (True):
+
+    # Updating the values of the distances to the silver and golds
     dist_silver , angle_silver = find_silver_token()
     dist_gold, angle_gold = find_golden_token()
     box = boxes(dist_silver, angle_silver)
 
+    # If the obstacles or nothing is detected
     if box is True:
-        if (dist_gold!=-1 and dist_gold<0.9):
+        # If there is any obstacles, and the distance is less than 1
+        if (dist_gold!=-1 and dist_gold < 1.0):
             print("Avoid obstacles")
-            rotate()            
+            rotate()    
+        # If nothing from both sides        
         else:
             print("Drive straight")
-            drive(100,0.2)
-            
+            drive(60,0.2)
+    
+    # If the silver box is detected
     elif (box is False):
+
+        # Turning to the direction of silver box
         if angle_silver < -a_th:
             print ("Rotate left")
             turn(-15, 0.1)
@@ -151,14 +168,16 @@ while (True):
             print ("Rotate right")
             turn(+15, 0.1)
             
-        elif(-a_th < angle_silver <a_th ):
+        elif (-a_th < angle_silver <a_th):
             print ("Move straight to catch it")
-            drive(80,0.15)
-            
+            drive(50, 0.15)
+        
+        # If the distance is close to grab the silver box
         if dist_silver < d_th:
             print ("Grab a silver box")
             R.grab()
             turn(25,2.5)
+            # drive(25,1)
             R.release()
             drive(-25,1)
             turn(-25,2.5)
