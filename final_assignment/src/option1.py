09a8d97b730516bb24132fc1a88460a15e40406a
BZWayne <bauyrzhan.zhakanov@gmail.com>
2022-02-10 23:34:58 +0100

    last

--- final_assignment/src/option1.py
+++ final_assignment/src/option1.py
@@ -38,13 +38,13 @@ def option_one():
 
         ## check and repeat until x is number
         check_num_x = is_number(x)
-        while not check_num_x:
+        while check_num_x is not True:
             x = float(input("Wrong input. Insert x coordinate: "))
 
         ## same procedure with y
         y = float(input("Insert y coordinate: "))
         check_num_y = is_number(y)
-        while not check_num_y:
+        while check_num_y is not True:
             x = float(input("Wrong input. Insert x coordinate: "))
 
         ## provide and publish move_base with (x,y) values
